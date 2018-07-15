3fbaf44675aa46fea8340467312f3e329a0df098
Tammy Metz <tammy@tammymetz.com>
2018-07-15 20:30:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence)
   word_array = sentence.split
   word_array.collect do |word|
     word.reverse
+    puts word.reverse
   end
   puts word_array.join(" ")
   word_array.join(" ")
